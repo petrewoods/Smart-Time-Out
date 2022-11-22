b3a7156f00e25a97eeff3ed7216bacd7b23b256f
Peter Woods <peter.woods@me.com>
2022-11-22 21:01:49 +0000

    Initial Commit

---
+++ Smart Time Out/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Smart Time Out
+//
+//  Created by Peter Woods on 22/11/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Smart Time Out/Smart_Time_OutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_Time_OutApp.swift
+//  Smart Time Out
+//
+//  Created by Peter Woods on 22/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Smart_Time_OutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
